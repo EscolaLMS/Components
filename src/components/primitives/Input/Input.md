9114e9a207275d8c2ea2ec4f52fc185638922544
Mateusz Qunabu <mateusz@qunabu.com>
2022-04-20 17:26:03 +0200

    input (#8)
    Co-authored-by: Mateusz Wojczal <mateusz@wojczal.com>

--- src/components/primitives/Input/Input.md
+++ src/components/primitives/Input/Input.md
@@ -1,12 +1,28 @@
 ```js
+import { useState } from "react";
 import { GlobalThemeProvider } from "../../../theme/provider";
 import ImageModal from "../../../styleguide/ImageModal";
 import img1 from "./Input.png";
 
+const [text, setText] = useState("lorem ipsum");
+
 <GlobalThemeProvider>
-  <Input>
-    <pre>This component is not ready yet</pre>
-  </Input>
+  <Input
+    placeholder="placeholder"
+    helper="helper"
+    type="email"
+    label="l;abel"
+    name="var"
+    value={text}
+    onChange={(e) => setText(e.target.value)}
+  />
+
+  <Input type="text" label="text" />
+
+  <Input type="text" label="disabled" value="disabled" disabled />
+
+  <Input type="text" label="readonly" value="readonly" readonly />
+
   <ImageModal images={[img1]} />
 </GlobalThemeProvider>;
 ```
--- src/components/primitives/Input/Input.tsx
+++ src/components/primitives/Input/Input.tsx
@@ -1,3 +1,49 @@
-import ContrastBox from "../../../components/primitives/ContrastBox/ContrastBox";
+import * as React from "react";
 
-export default ContrastBox;
+import styled, { withTheme } from "styled-components";
+import theme from "theme";
+import { getFontFromTheme } from "../../../theme/provider";
+
+export interface InputProps
+  extends Omit<React.InputHTMLAttributes<HTMLInputElement>, "type"> {
+  label?: React.ReactNode;
+  helper?: React.ReactNode;
+  type: "email" | "number" | "password" | "search" | "tel" | "text" | "url";
+}
+
+// Main button with styles
+const StyledDiv = styled("div")<InputProps>`
+  font-family: ${(props) => getFontFromTheme(props.theme).fontFamily};
+  input {
+    font-family: ${(props) => getFontFromTheme(props.theme).fontFamily};
+  }
+`;
+
+// Main button with styles
+export const Input: React.FC<InputProps> = ({
+  label,
+  helper,
+  type,
+  ...props
+}) => {
+  if (label) {
+    return (
+      <StyledDiv type={type}>
+        <label>
+          <span>{label}</span>
+          <input type={type} {...Object.assign(props, { theme: undefined })} />
+          {helper && <span>{helper}</span>}
+        </label>
+      </StyledDiv>
+    );
+  }
+  return (
+    <StyledDiv type={type}>
+      <input type={type} {...props} />
+    </StyledDiv>
+  );
+};
+
+const NewInput = styled(Input)<InputProps>``;
+
+export default withTheme(NewInput);
