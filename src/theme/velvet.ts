51927ce7a41fc32ce5b2c4cf25b86b6505d44bbc
Przemysław Ostrowiński <p.ostrowinski@gmail.com>
2022-04-26 12:05:27 +0200

    LMSDEMO - 27 - add style to download anchor (#9)
    * LMSDEMO - 27 - add style to download anchor
    
    * LMSDEMO - 27 - change donwload button colors
    
    * LMSDEMO - 27 - fixes after review
    
    * LMSDEMO - 27 - added default themes with dark and light mode and add more theme live customization props
    
    Co-authored-by: postrowi <przemyslaw.ostrowinski@escolasoft.com>

--- src/theme/velvet.ts
+++ src/theme/velvet.ts
@@ -1,15 +1,13 @@
 import { DefaultTheme } from "styled-components";
 import { blend } from "chroma-js";
+import { sharedTheme } from "./shared";
 
 export const velvetTheme: DefaultTheme = {
+  ...sharedTheme,
   primaryColor: "#600CAE",
   secondaryColor: blend("#600CAE", "#BDBDBD", "multiply").hex(),
   font: "Mulish",
-  buttonRadius: 0,
-  checkboxRadius: 0,
   headerColor: "#111111",
-  backgroundDark: "#4A4A4A",
-  backgroundLight: "#FFFFFF",
 };
 
 export default velvetTheme;
