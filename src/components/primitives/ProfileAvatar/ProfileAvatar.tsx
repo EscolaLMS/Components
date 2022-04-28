dfa217e29f107d6c0a855c6840f5829f118ba4ab
Piotr <piotr.kananowicz@icloud.com>
2022-04-28 21:24:51 +0200

    Update src/components/primitives/ProfileAvatar/ProfileAvatar.tsx
    Co-authored-by: Mateusz Qunabu <mateusz@qunabu.com>

--- src/components/primitives/ProfileAvatar/ProfileAvatar.tsx
+++ src/components/primitives/ProfileAvatar/ProfileAvatar.tsx
@@ -9,26 +9,8 @@ export interface ProfileAvatarProps
 
 const StyledProfileAvatar = styled("img")<ProfileAvatarProps>`
   border-radius: 50%;
-  width: ${(props) => {
-    if (props.size) {
-      switch (props.size) {
-        case "large":
-          return "116px";
-        default:
-          return "50px";
-      }
-    }
-  }};
-  height: ${(props) => {
-    if (props.size) {
-      switch (props.size) {
-        case "large":
-          return "116px";
-        default:
-          return "50px";
-      }
-    }
-  }};
+  width: ${(props) => prop.size === "large" ? "116px" : "50px",
+  height: ${(props) => prop.size === "large" ? "116px" : "50px",
 `;
 
 export const ProfileAvatar: React.FC<ProfileAvatarProps> = ({
