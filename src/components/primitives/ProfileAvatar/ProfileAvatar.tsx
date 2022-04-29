d9d84f8a008613990008b1ee34d36c443ebecc11
Piotr <piotr.kananowicz@icloud.com>
2022-04-29 14:33:01 +0200

    LMSDEMO-18 - ProfileAvatar (#18)
    * LMSDEMO-18 - ProfileAvatar
    
    * remove already prepared props
    
    * Update src/components/primitives/ProfileAvatar/ProfileAvatar.tsx
    
    Co-authored-by: Mateusz Qunabu <mateusz@qunabu.com>
    
    * Mateusz's remarks
    
    * refactor: add extra sizes + DRY
    
    * refactor: rename + move to right directories
    
    * refactor: indentation
    
    * fix: Mateusz's remarks
    
    * refactor: indentation
    
    Co-authored-by: Mateusz Qunabu <mateusz@qunabu.com>

--- src/components/primitives/ProfileAvatar/ProfileAvatar.tsx
+++ src/components/primitives/ProfileAvatar/ProfileAvatar.tsx
@@ -1,3 +1,29 @@
-import ContrastBox from "../../../styleguide/ContrastBox/ContrastBox";
+import * as React from "react";
 
-export default ContrastBox;
+import styled, { withTheme } from "styled-components";
+import {ProfileAvatarTypesStr} from "../../../types/ProfileAvatarTypes";
+import {setProfileAvatarBySize} from "../../../utils/components/primitives/profileAvatarUtils";
+
+export interface ProfileAvatarProps
+  extends React.ImgHTMLAttributes<HTMLImageElement> {
+  size?: ProfileAvatarTypesStr;
+}
+
+const StyledProfileAvatar = styled("img")<ProfileAvatarProps>`
+  border-radius: 50%;
+  width: ${props => setProfileAvatarBySize(props.size)};
+  height: ${props => setProfileAvatarBySize(props.size)};
+`;
+
+export const ProfileAvatar: React.FC<ProfileAvatarProps> = ({
+  src,
+  alt,
+  size = "medium",
+  style
+}) => {
+  return <StyledProfileAvatar src={src} alt={alt} size={size} style={style} />;
+};
+
+const NewProfileAvatar = styled(ProfileAvatar)<{ size: string }>``;
+
+export default withTheme(NewProfileAvatar);
