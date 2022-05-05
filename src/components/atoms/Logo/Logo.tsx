166e4d6d0b2bacf303125eab27827632a3581ba6
Przemysław Ostrowiński <p.ostrowinski@gmail.com>
2022-05-05 15:11:12 +0200

    LMSDEMO - 12 - add styled logo component (#32)
    * LMSDEMO - 12 - add styled logo component
    
    * LMSDEMO - 12 - add logo images after merge
    
    Co-authored-by: postrowi <przemyslaw.ostrowinski@escolasoft.com>

--- src/components/atoms/Logo/Logo.tsx
+++ src/components/atoms/Logo/Logo.tsx
@@ -1,3 +1,17 @@
-import ContrastBox from "../../../styleguide/ContrastBox/ContrastBox";
+import * as React from "react";
+import styled, { withTheme } from "styled-components";
 
-export default ContrastBox;
+export interface LogoProps extends React.ImgHTMLAttributes<HTMLImageElement> {
+  isSmall?: boolean;
+}
+
+const StyledLogo = styled("img")<LogoProps>`
+  max-width: ${(props) => (props.isSmall ? "80px" : "120px")};
+  max-height: ${(props) => (props.isSmall ? "50px" : "75px")};
+`;
+
+export const Logo: React.FC<LogoProps> = (props) => <StyledLogo {...props} />;
+
+const NewStyledLogo = styled(Logo)<LogoProps>``;
+
+export default withTheme(NewStyledLogo);
