4fc2c612436b27efa885c47773ea50a1cbfd5ec5
Piotr <piotr.kananowicz@icloud.com>
2022-05-06 16:31:15 +0200

    LMSDEMO-20 - Switcher (#37)
    * LMSDEMO-20 - Switcher
    
    * revert changes
    
    * refactor: remove unused import

--- src/components/molecules/Switcher/Switcher.tsx
+++ src/components/molecules/Switcher/Switcher.tsx
@@ -1,3 +1,131 @@
-import ContrastBox from "../../../styleguide/ContrastBox/ContrastBox";
+import * as React from "react";
 
-export default ContrastBox;
+import styled, { withTheme } from "styled-components";
+import { Radio } from "../../atoms/Option/Radio";
+
+export interface SwitcherOptionProps {
+  value: string;
+  label: string;
+  checked?: boolean;
+  onChange: (value: string) => void;
+}
+
+export interface SwitcherProps extends React.HTMLAttributes<HTMLDivElement> {
+  options: SwitcherOptionProps[];
+  checked: number;
+}
+
+const StyledSwitcher = styled("div")<SwitcherProps>`
+  position: relative;
+  display: flex;
+  width: 100%;
+
+  &:before {
+    width: 100%;
+    background-color: ${({ theme }) => theme.gray3};
+  }
+
+  &:before,
+  .progress-bar {
+    position: absolute;
+    left: 0;
+    bottom: 13px;
+    content: "";
+    display: block;
+    height: 1px;
+  }
+
+  .progress-bar {
+    transition: width 0.2s ease-in-out;
+    background-color: ${({ theme }) => theme.primaryColor};
+  }
+`;
+
+const StyledSwitcherOption = styled("div")`
+  position: relative;
+  padding-top: 30px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  flex: 1;
+
+  label {
+    position: relative;
+
+    &:hover span {
+      opacity: 1;
+    }
+  }
+
+  span {
+    top: -30px;
+    margin: 0;
+    position: absolute;
+    left: 10px;
+    transform: translateX(-50%);
+    color: ${(props) => {
+      return props.theme.mode !== "light"
+        ? props.theme.white
+        : props.theme.gray1;
+    }};
+    font-weight: bold;
+    white-space: nowrap;
+    opacity: 0;
+    transition: opacity 0.2s ease-in-out;
+  }
+
+  input:checked + span {
+    opacity: 1;
+  }
+
+  &:last-child {
+    span {
+      left: auto;
+      right: 0;
+      transform: translateX(0);
+    }
+  }
+`;
+
+const SwitcherOption: React.FC<SwitcherOptionProps> = (props) => {
+  const { value, label, checked } = props;
+
+  return (
+    <StyledSwitcherOption>
+      <Radio
+        value={value}
+        checked={checked}
+        label={label}
+        onChange={() => props.onChange(value)}
+      />
+    </StyledSwitcherOption>
+  );
+};
+
+export const Switcher: React.FC<SwitcherProps> = (props) => {
+  const { options, checked } = props;
+  const [checkedOption, setCheckedOption] = React.useState(checked || 0);
+
+  const progressBarWidth = `${Math.round(
+    ((checkedOption + 1) / options.length) * 100
+  )}%`;
+
+  return (
+    <StyledSwitcher options={options} checked={checked}>
+      <div className={"progress-bar"} style={{ width: progressBarWidth }} />
+      {options.map((option, index) => (
+        <SwitcherOption
+          key={option.value}
+          value={option.value}
+          label={option.label}
+          checked={index === checkedOption}
+          onChange={() => setCheckedOption(index)}
+        />
+      ))}
+    </StyledSwitcher>
+  );
+};
+
+const NewSwitcher = styled(Switcher)<SwitcherProps>``;
+
+export default withTheme(NewSwitcher);
