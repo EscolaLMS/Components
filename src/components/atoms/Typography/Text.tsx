1148b88cf5296bc23c706f41ad671ed37208cfa8
Piotr <piotr.kananowicz@icloud.com>
2022-05-10 13:30:22 +0200

    LMSDEMO-36 - Certificates (#50)
    * LMSDEMO-36 - Certificates
    
    * update border color + fix ident
    
    * remove property
    
    * use elastic handlers

--- src/components/atoms/Typography/Text.tsx
+++ src/components/atoms/Typography/Text.tsx
@@ -5,8 +5,9 @@ import styled, { withTheme } from "styled-components";
 import { getFontFromTheme } from "../../../theme/provider";
 
 export interface TitleProps extends React.HTMLAttributes<HTMLHeadingElement> {
-  children?: React.ReactNode;
   noMargin?: boolean;
+  bold?: boolean;
+  size?: "16" | "14";
 }
 
 const StyledP = styled.p<TitleProps>`
@@ -15,15 +16,15 @@ const StyledP = styled.p<TitleProps>`
   color: ${(props) =>
     props.theme.mode !== "light" ? props.theme.white : "#111"};
   font-family: ${(props) => getFontFromTheme(props.theme).fontFamily};
-  font-weight: normal;
-  font-size: 16px;
+  font-weight: ${(props) => (props.bold ? "bold" : "normal")};
+  font-size: ${(props) => (props.size === "14" ? "14px" : "16px")};
   line-height: 1.55em;
 `;
 
 export const Text: React.FC<TitleProps> = (props) => {
-  const { children, noMargin, style } = props;
+  const { children, noMargin, style, bold, size = "16" } = props;
   return (
-    <StyledP style={style} noMargin={noMargin}>
+    <StyledP style={style} noMargin={noMargin} bold={bold} size={size}>
       {children}
     </StyledP>
   );
