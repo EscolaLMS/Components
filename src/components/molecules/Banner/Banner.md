9d7242196bdfc4e3a297e41e8e8ae150b964e724
pieecia <piotr.kananowicz@escolasoft.com>
2022-05-11 09:08:07 +0200

    LMSDEMO-36 - Banner

--- src/components/molecules/Banner/Banner.md
+++ src/components/molecules/Banner/Banner.md
@@ -2,11 +2,38 @@
 import { GlobalThemeProvider } from "../../../theme/provider";
 import ImageModal from "../../../styleguide/ImageModal";
 import img1 from "./Banner.png";
+import img2 from "./BannerIllustration.png";
 
 <GlobalThemeProvider>
-  <Banner>
-    <pre>This component is not ready yet</pre>
-  </Banner>
+  <div style={{ width: "100%" }}>
+    <Banner
+      text={"My markdown text with some **bold**"}
+      btnText={"Sprawdź przycisk"}
+      handleBtn={() => {
+        console.log("clicked");
+      }}
+      img={{
+        src: img2,
+        alt: "Custom Alt",
+        title: "Custom Title",
+      }}
+    />
+  </div>
+  <div style={{ width: "576px" }}>
+    <Banner
+      mobile={true}
+      text={"My markdown text with some **bold**"}
+      btnText={"Sprawdź przycisk"}
+      handleBtn={() => {
+        console.log("clicked");
+      }}
+      img={{
+        src: img2,
+        alt: "Custom Alt",
+        title: "Custom Title",
+      }}
+    />
+  </div>
   <ImageModal images={[img1]} />
 </GlobalThemeProvider>;
 ```
--- src/components/molecules/Banner/Banner.tsx
+++ src/components/molecules/Banner/Banner.tsx
@@ -1,3 +1,66 @@
-import ContrastBox from "../../../styleguide/ContrastBox/ContrastBox";
+import * as React from "react";
+import ReactMarkdown from "react-markdown";
+import styled, { withTheme } from "styled-components";
+import { Row, Col } from "react-grid-system";
+import { Title } from "../../atoms/Typography/Title";
+import { Button } from "../../atoms/Button/Button";
 
-export default ContrastBox;
+export interface BannerImgProps {
+  src: string;
+  alt?: string;
+  title?: string;
+}
+
+export interface BannerProps extends React.HTMLAttributes<HTMLDivElement> {
+  text: string;
+  btnText: string;
+  img: BannerImgProps;
+  handleBtn: () => void;
+  mobile?: boolean;
+}
+
+const StyledBanner = styled("div")<BannerProps>`
+  .banner-btn {
+    margin-top: ${(props) => (props.mobile ? "22px" : "52px")};
+  }
+
+  .banner-img {
+    max-width: 100%;
+    height: 100%;
+  }
+`;
+
+export const Banner: React.FC<BannerProps> = (props) => {
+  const { text, img, btnText, handleBtn, mobile = false } = props;
+
+  return (
+    <StyledBanner {...props}>
+      <Row align={"center"} direction={mobile ? "column-reverse" : "row"}>
+        <Col xs={12} md={mobile ? 12 : 6}>
+          <Title
+            level={mobile ? 3 : 1}
+            as={mobile ? "h3" : "h1"}
+            style={{ fontWeight: "normal" }}
+          >
+            <ReactMarkdown components={{ p: React.Fragment }} children={text} />
+          </Title>
+          <Button className={"banner-btn"} onClick={handleBtn}>
+            {btnText}
+          </Button>
+        </Col>
+        <Col xs={12} md={mobile ? 12 : 6}>
+          <img
+            src={img.src}
+            alt={img.alt}
+            title={img.title}
+            className={"banner-img"}
+          />
+        </Col>
+      </Row>
+    </StyledBanner>
+  );
+};
+
+const NewBanner = styled(Banner)<BannerProps>``;
+
+export default withTheme(NewBanner);
