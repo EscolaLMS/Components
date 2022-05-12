e6c301ea06745de43a90d8020ed1113ac6b319f6
pieecia <piotr.kananowicz@escolasoft.com>
2022-05-12 10:40:56 +0200

    LMSDEMO-108 - Icon Text

---
+++ src/components/atoms/IconText/IconText.tsx
@@ -0,0 +1,62 @@
+import * as React from "react";
+import styled, { withTheme } from "styled-components";
+import { getFontFromTheme } from "../../../theme/provider";
+
+interface Styles {
+  icon?: React.CSSProperties;
+  text?: React.CSSProperties;
+}
+
+export interface IconTextProps
+  extends React.HTMLAttributes<HTMLParagraphElement> {
+  icon: React.ReactNode;
+  text: string;
+  styles?: Styles;
+}
+
+const StyledText = styled("p")<IconTextProps>`
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  margin: 0 0 20px 0;
+  font-family: ${(props) => getFontFromTheme(props.theme).fontFamily};
+  display: flex;
+  flex-wrap: wrap;
+  font-size: 14px;
+  color: ${(props) => {
+    return props.theme.mode !== "light" ? props.theme.white : props.theme.gray1;
+  }};
+
+  .icon {
+    width: 1em;
+    display: flex;
+    align-items: center;
+    margin-right: 9px;
+    svg {
+      fill: ${(props) => {
+        return props.theme.mode !== "light"
+          ? props.theme.white
+          : props.theme.black;
+      }};
+      flex-shrink: 0;
+    }
+  }
+`;
+
+export const IconText: React.FC<IconTextProps> = (props) => {
+  const { text, icon, styles } = props;
+
+  return (
+    <StyledText {...props}>
+      <span className="icon" style={styles?.icon}>
+        {icon}
+      </span>
+      <span className="title" style={styles?.text}>
+        {text}
+      </span>
+    </StyledText>
+  );
+};
+
+const NewComponent = styled(IconText)<IconTextProps>``;
+
+export default withTheme(NewComponent);
--- src/components/atoms/Typography/Text.tsx
+++ src/components/atoms/Typography/Text.tsx
@@ -4,14 +4,14 @@ import styled, { withTheme } from "styled-components";
 
 import { getFontFromTheme } from "../../../theme/provider";
 
-export interface TitleProps extends React.HTMLAttributes<HTMLHeadingElement> {
+export interface TextProps extends React.HTMLAttributes<HTMLParagraphElement> {
   noMargin?: boolean;
   bold?: boolean;
   size?: "16" | "14";
   type?: "primary" | "secondary" | "warning" | "danger";
 }
 
-const StyledP = styled.p<TitleProps>`
+const StyledP = styled.p<TextProps>`
   margin: ${(props) => (props.noMargin ? "0" : "0 0 1.55em 0")};
   padding: 0;
   color: ${(props) => {
@@ -29,7 +29,7 @@ const StyledP = styled.p<TitleProps>`
   line-height: 1.55em;
 `;
 
-export const Text: React.FC<TitleProps> = (props) => {
+export const Text: React.FC<TextProps> = (props) => {
   const {
     children,
     noMargin,
