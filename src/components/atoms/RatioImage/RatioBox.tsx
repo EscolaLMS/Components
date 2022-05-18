ab935d073eed49fc88904c1ba17d641926b70868
Mateusz Wojczal <mateusz@wojczal.com>
2022-05-18 14:22:36 +0200

    lint fix

--- src/components/atoms/RatioImage/RatioBox.tsx
+++ src/components/atoms/RatioImage/RatioBox.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import styled, { withTheme } from "styled-components";
+import styled from "styled-components";
 
 export interface RatioBoxProps {
   ratio: number;
