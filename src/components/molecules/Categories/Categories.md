30e2f447e20d31a5702ad77d33f45bb25957f111
postrowi <przemyslaw.ostrowinski@escolasoft.com>
2022-05-26 13:25:05 +0200

    LMSDEMO - 92 - copy old categories and fix bug

--- src/components/molecules/Categories/Categories.md
+++ src/components/molecules/Categories/Categories.md
@@ -3,11 +3,10 @@ import { GlobalThemeProvider } from "../../../theme/provider";
 import ImageModal from "../../../styleguide/ImageModal";
 import img1 from "./Categories.png";
 import img2 from "./Categories-2.png";
+import json from "./mock.json";
 
 <GlobalThemeProvider>
-  <Categories>
-    <pre>This component is not ready yet</pre>
-  </Categories>
+  <Categories categories={json.data} onChange={(val) => console.log(val)} />
   <ImageModal images={[img1, img2]} />
 </GlobalThemeProvider>;
 ```
--- src/components/molecules/Categories/Categories.tsx
+++ src/components/molecules/Categories/Categories.tsx
@@ -1,3 +1,69 @@
-import ContrastBox from "../../../styleguide/ContrastBox/ContrastBox";
+import * as React from "react";
+import type { Category } from "@escolalms/sdk/lib/types/api";
+import { useTranslation } from "react-i18next";
 
-export default ContrastBox;
+interface CategoryTreeOptionsProps {
+  categories: Category[];
+  id?: number;
+  labelPrefix?: string;
+}
+
+const CategoryTreeOptions: React.FC<CategoryTreeOptionsProps> = ({
+  categories,
+  id,
+  labelPrefix,
+}) => {
+  return (
+    <React.Fragment>
+      {categories
+        // .filter((category) => category.count && category.count > 1)
+        .map((category: Category) => (
+          <React.Fragment key={category.id}>
+            <option value={category.id} selected={Number(id) === category.id}>
+              {labelPrefix ? `${labelPrefix}${category.name}` : category.name}
+            </option>
+            {category &&
+              category.subcategories &&
+              category.subcategories.length > 0 && (
+                <CategoryTreeOptions
+                  labelPrefix={
+                    labelPrefix
+                      ? `${labelPrefix}${category.name} > `
+                      : `${category.name} > `
+                  }
+                  categories={category.subcategories}
+                  id={id}
+                />
+              )}
+          </React.Fragment>
+        ))}
+    </React.Fragment>
+  );
+};
+
+interface CategoriesProps {
+  categories: Category[];
+  onChange: (value: string) => void;
+  id?: number;
+}
+
+export const Categories: React.FC<CategoriesProps> = (props) => {
+  const { categories, id, onChange } = props;
+  const { t } = useTranslation();
+
+  return (
+    <div className="select-box">
+      <select
+        className="form-control"
+        onChange={(e) => {
+          onChange && onChange(e.target.value);
+        }}
+      >
+        <option value="" selected={!!id}>
+          {t("All Categories")}
+        </option>
+        <CategoryTreeOptions categories={categories} id={id} />
+      </select>
+    </div>
+  );
+};
