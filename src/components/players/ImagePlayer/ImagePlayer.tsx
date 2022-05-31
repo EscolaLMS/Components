30c86991046830bec6c76c8e7425f5765ac461e7
Przemek Wiktorski <przemyslaw.wiktorskii@gmail.com>
2022-05-31 09:30:55 +0200

    Image player Feature/lmsdemo 118 (#117)
    * Imag eplayer
    
    * player
    
    * player
    
    * Image Player
    
    * Image Player
    
    * Image Player
    
    * removed ununsed import
    
    Co-authored-by: Przemek <przemek.wiktorski@escola.pl>

---
+++ src/components/players/ImagePlayer/ImagePlayer.tsx
@@ -0,0 +1,58 @@
+import * as React from "react";
+import { useCallback } from "react";
+import Lightbox from "react-image-lightbox";
+import styled, { withTheme } from "styled-components";
+import Image from "@escolalms/sdk/lib/react/components/Image";
+import { API } from "@escolalms/sdk/lib";
+import { EscolaLMSContext } from "@escolalms/sdk/lib/react";
+import "react-image-lightbox/style.css";
+
+const StyledImagePlayer = styled("div")`
+  > div {
+    cursor: pointer;
+
+    img {
+      width: 100%;
+      transition: transform 0.5s ease-out;
+    }
+    &:hover {
+      img {
+        transform: scale(1.02);
+      }
+    }
+  }
+`;
+
+interface ImagePlayerProps {
+  topic: API.TopicImage;
+  onLoad: () => void;
+}
+
+export const ImagePlayer: React.FC<ImagePlayerProps> = ({ topic, onLoad }) => {
+  const [open, setOpen] = React.useState<boolean>(false);
+  const onCloseRequest = useCallback(() => setOpen(false), []);
+  const { apiUrl } = React.useContext(EscolaLMSContext);
+
+  React.useEffect(() => {
+    onLoad();
+  }, []);
+
+  return (
+    <StyledImagePlayer>
+      <Image
+        path={topic.topicable.value}
+        srcSizes={[500, 750, 1000]}
+        onClick={() => setOpen(true)}
+      />
+      {open && (
+        <Lightbox
+          wrapperClassName=""
+          mainSrc={`${apiUrl}/api/images/img?path=${topic.topicable.value}`}
+          onCloseRequest={onCloseRequest}
+        />
+      )}
+    </StyledImagePlayer>
+  );
+};
+
+export default withTheme(styled(ImagePlayer)<ImagePlayerProps>``);
