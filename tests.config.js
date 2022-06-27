8c4cbc8cbd747284bf9190bd3f3e8ad231789045
pieecia <piotr.kananowicz@escolasoft.com>
2022-06-27 16:35:40 +0200

    github actions

--- tests.config.js
+++ tests.config.js
@@ -1,5 +1,9 @@
 const React = require("react");
 const ReactDOM = require("react-dom");
 
+const context = {
+    include: [['[data-preview]']]
+}
+
 const axe = require("@axe-core/react");
-axe(React, ReactDOM, 1000);
+axe(React, ReactDOM, 1000, undefined, context);
