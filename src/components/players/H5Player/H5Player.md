8361cd7361b50f89f1719dd69fea56b6cb8672d2
Mateusz Motyka <mati@mateuszs-mini.home>
2022-07-05 14:00:00 +0200

    basic styles for h5p players

--- src/components/players/H5Player/H5Player.md
+++ src/components/players/H5Player/H5Player.md
@@ -3,11 +3,19 @@ import { GlobalThemeProvider } from "../../../theme/provider";
 import ImageModal from "../../../styleguide/ImageModal";
 import { EscolaLMSContextProvider } from "@escolalms/sdk/lib/react/context";
 import ThemeTester from "../../../styleguide/ThemeTester";
+import { Button, Title, Text } from "../../..";
 
 <React.Fragment>
   <EscolaLMSContextProvider apiUrl="https://api-stage.escolalms.com/">
-    <ThemeTester>
-      <H5Player onXAPI={(e) => console.log(e)} id={"4"} />
+    <ThemeTester flexDirection="column">
+      <div style={{ marginBottom: 30, width: "100%" }}>
+        <Title level={4}>Essay</Title>
+        <H5Player onXAPI={(e) => console.log(e)} id={"74"} />
+      </div>
+      <div style={{ marginBottom: 30, width: "100%" }}>
+        <Title level={4}>Dialog Cards</Title>
+        <H5Player onXAPI={(e) => console.log(e)} id={"7"} />
+      </div>
     </ThemeTester>
   </EscolaLMSContextProvider>
 </React.Fragment>;
--- src/components/players/H5Player/H5Player.tsx
+++ src/components/players/H5Player/H5Player.tsx
@@ -4,6 +4,7 @@ import { XAPIEvent, Player } from "@escolalms/h5p-react";
 
 import * as API from "@escolalms/sdk/lib/types/api";
 import { EscolaLMSContext } from "@escolalms/sdk/lib/react/context";
+import theme from "theme";
 
 const StyledH5P = styled("div")`
   border-radius: ${(props) => props.theme.buttonRadius || 0}px;
@@ -31,14 +32,218 @@ export const H5Player: React.FC<H5PProps> = ({
   }, [id, fetchH5P]);
 
   const themeContext = useContext(ThemeContext);
+  const fontColor =
+    themeContext.mode === "dark" ? themeContext.white : themeContext.black;
+  const backgroundColor =
+    themeContext.mode === "dark"
+      ? themeContext.backgroundDark
+      : themeContext.backgroundLight;
+  const inputBackground =
+    themeContext.mode === "dark" ? "transparent" : themeContext.gray5;
+  const inputBorder =
+    themeContext.mode === "dark" ? themeContext.white : themeContext.gray1;
 
   const h5pThemeCSSOverwriteSrc = useMemo(() => {
-    // TODO - this is a hack to get the theme css to work. reolace this with actual css
     const css = `
     .h5p-baq-intro-page {
       background: ${themeContext.secondaryColor} !important;
       color: ${themeContext.primaryColor} !important;
     }
+    .h5p-joubelui-button.mq-control-button {
+      background: ${themeContext.primaryColor} !important;
+      border-bottom: none!important;
+      text-shadow: none!important;
+    }
+    .h5p-baq-intro-page-title {
+      text-shadow: none!important;
+      color: ${themeContext.white} !important;
+    }
+    .h5p-baq {
+      background: ${themeContext.primaryColor} !important;
+    }
+    .h5p-baq-countdown-text {
+      background: ${themeContext.primaryColor} !important;
+    }
+    .h5p-baq-countdown-bg.fuel {
+      background: ${themeContext.primaryColor} !important;
+      filter: brightness(0.8) !important;
+    }
+    .h5p-joubelui-progressbar-background {
+      background: ${themeContext.primaryColor} !important;
+      filter: brightness(0.8) !important;
+    }
+    .h5p-baq-alternatives > .h5p-joubelui-button:active, .h5p-baq-alternatives > .h5p-joubelui-button:hover {
+      background: ${themeContext.primaryColor} !important;
+      filter: brightness(0.8) !important;
+    }
+   .odometer-value {
+    color: ${themeContext.primaryColor} !important;
+   }
+   .h5p-question {
+    background: ${backgroundColor} !important;
+    color: ${fontColor}!important;
+   }
+   .h5peditor .ui-dialog .h5p-joubelui-button, .h5peditor .h5p-joubelui-button, .h5p-joubelui-button {
+    background: ${themeContext.primaryColor} !important;
+   }
+   .h5p-crossword-cell.h5p-crossword-cell-empty {
+    background: ${
+      themeContext.mode === "dark" ? themeContext.gray2 : themeContext.gray3
+    } !important;
+   }
+   .h5p-crossword-input-fields-group-input {
+    background:${inputBackground} !important;
+    border: 1px solid ${inputBorder}!important;
+    color: ${fontColor} !important;
+   }
+   .h5p-crossword .h5p-crossword-cell:not(.h5p-crossword-solution-correct):not(.h5p-crossword-solution-wrong):not(.h5p-crossword-solution-neutral).h5p-crossword-highlight-normal {
+    background: ${themeContext.primaryColor}!important;
+   }
+   .h5p-crossword .h5p-crossword-input-fields-group-wrapper-clue.h5p-crossword-input-fields-group-clue-highlight-focus .h5p-crossword-input-fields-group-clue-id {
+    background: ${themeContext.primaryColor}!important;
+    color: ${themeContext.white}!important;
+   }
+   .h5p-crossword .h5p-crossword-cell.h5p-crossword-highlight-normal .h5p-crossword-cell-canvas {
+    color: ${themeContext.white}!important;
+   }
+   .h5p-dialogcards {
+      background: ${backgroundColor} !important;
+      color: ${fontColor}!important;
+   }
+   .h5p-dialogcards-card-content {
+    background: ${backgroundColor} !important;
+   }
+   .h5p-dialogcards .h5p-audio-minimal-button {
+    background: ${themeContext.primaryColor}!important;
+   }
+   .h5p-essay-input-field-textfield {
+    background: ${inputBackground} !important;
+    border: 1px solid ${inputBorder}!important;
+    color: ${fontColor}!important;
+   }
+   .h5p-question-feedback-content-text {
+    color: ${themeContext.primaryColor}!important;
+   }
+   .h5p-question-explanation-container {
+    background: ${backgroundColor} !important;
+   }
+   .h5p-question-explanation-item {
+    background: ${backgroundColor} !important;
+   }
+   .h5p-accordion {
+    background: ${backgroundColor} !important;
+    color: ${fontColor}!important;
+   }
+   .h5p-panel-title {
+    color: ${fontColor}!important;
+   }
+   .h5p-panel-expanded {
+    color: ${themeContext.primaryColor}!important;
+   }
+   .h5p-questionnaire-element.h5p-questionnaire-required .h5p-subcontent-question {
+    background: ${themeContext.primaryColor}!important;
+    color: ${themeContext.white}!important;
+   }
+   .h5p-questionnaire-required-symbol {
+    background: ${themeContext.primaryColor}!important;
+   }
+   .h5p-questionnaire-progress-bar-current {
+    background: ${themeContext.primaryColor} !important;
+    filter: brightness(0.8) !important;
+   }
+   .h5p-questionnaire-footer {
+    background: ${
+      themeContext.mode === "dark" ? themeContext.gray2 : themeContext.gray4
+    } !important;
+    border: none!important;
+   }
+   .h5p-questionnaire-button, .h5peditor .h5p-questionnaire-button {
+    background: ${themeContext.primaryColor} !important;
+    border: none!important;
+   }
+   .h5p-open-ended-question-input {
+    background: ${inputBackground} !important;
+    border: 1px solid ${inputBorder}!important;
+    color: ${fontColor}!important;
+    width: 100%!important;
+    max-width: 100%!important;
+   }
+   .h5p-open-ended-question-content {
+    background: ${backgroundColor} !important;
+   }
+   .h5p-open-ended-question-question:before {
+    background: ${backgroundColor} !important;
+   }
+   .h5p-questionnaire-progress-bar {
+    background: ${themeContext.gray2} !important;
+   }
+   .h5p-open-ended-question-question, 
+   .h5p-simple-multiple-choice-question, 
+   h5p-subcontent-question {
+    background: ${themeContext.primaryColor} !important;
+   }
+   .h5p-true-false-answer {
+    background: ${backgroundColor}!important;
+   }
+   .h5p-true-false-answer[aria-checked=true] {
+    background: ${themeContext.primaryColor}!important;
+    color: ${themeContext.white}!important;
+    border: 1px solid ${themeContext.primaryColor}!important;
+   }
+   .h5p-true-false-answer:focus {
+    box-shadow: 0 0 0 1px ${themeContext.primaryColor}!important;
+   }
+   .h5p-multichoice .h5p-alternative-container {
+    background: ${inputBackground} !important;
+    border: 1px solid ${inputBorder}!important;
+    box-shadow: none!important;
+   }
+   .h5p-multichoice .h5p-answer[aria-checked="true"] .h5p-alternative-container {
+    color: ${themeContext.primaryColor}!important;
+   }
+   .h5p-multichoice .h5p-answer .h5p-alternative-container:before {
+    color: ${
+      themeContext.mode === "dark" ? themeContext.gray4 : themeContext.gray2
+    }!important;
+   }
+   .h5p-question-feedback {
+    color: ${themeContext.primaryColor}!important;
+   }
+   ul.h5p-sc-alternatives li.h5p-sc-alternative {
+    background: ${inputBackground} !important;
+    border: 1px solid ${inputBorder}!important;
+    box-shadow: none!important;
+   }
+   li.h5p-sc-alternative .h5p-sc-progressbar {
+    background: ${themeContext.primaryColor}!important;
+   }
+  
+   ul.h5p-sc-alternatives.h5p-sc-selected li.h5p-sc-alternative.h5p-sc-reveal-correct, 
+   ul.h5p-sc-alternatives.h5p-sc-selected li.h5p-sc-alternative.h5p-sc-reveal-correct:hover, 
+   ul.h5p-sc-alternatives.h5p-sc-selected li.h5p-sc-alternative.h5p-sc-reveal-correct:active, 
+   ul.h5p-sc-alternatives.h5p-sc-selected li.h5p-sc-alternative.h5p-sc-reveal-correct:focus {
+    color: ${themeContext.mode === "dark" ? "lime" : "green"}!important;
+   }
+   .h5p-image-hotspot-popup {
+    background: ${backgroundColor} !important;
+    color: ${fontColor}!important;
+   }
+   .h5p-image-hotspots {
+    background-color: ${backgroundColor} !important;
+   }
+   .h5p-image-hotspot-popup-pointer {
+    border-left: 0.6em solid ${backgroundColor}!important;
+   }
+   .h5p-image-sequencing {
+    background: ${backgroundColor} !important;
+    color: ${fontColor}!important;
+   }
+   .h5p-task-description {
+    color: ${fontColor}!important;
+   }
+   .draggabled .image-desc .text {
+    color: ${fontColor}!important;
+   }
   `;
     return `data:text/css;base64,${btoa(css)}`;
   }, [themeContext]);
