19ee99fddfa7d825aa9716ff33dc58412bc78065
pieecia <piotr.kananowicz@escolasoft.com>
2022-07-11 12:59:36 +0200

    LMSDEMO-421 - Bold current lesson

--- src/components/organisms/CourseAgenda/CourseAgenda.tsx
+++ src/components/organisms/CourseAgenda/CourseAgenda.tsx
@@ -344,10 +344,15 @@ const CourseAgendaTopic: React.FC<CourseAgendaTopicProps> = ({
       >
         <div className={"lesson__description"}>
           <TopicIcon mode={mode} />
-          <Text className={"lesson__index"} size={"14"} noMargin>
+          <Text
+            className={"lesson__index"}
+            size={"14"}
+            noMargin
+            bold={mode === "current"}
+          >
             {index}.{" "}
           </Text>
-          <Text size={"14"} noMargin>
+          <Text size={"14"} noMargin bold={mode === "current"}>
             {topic.title}
           </Text>
         </div>
